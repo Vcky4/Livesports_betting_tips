c7bfdbb277da089154f9227ac13815dba71987a4
Vcky4 <okon.victor.u@gmail.com>
2023-07-19 15:40:48 +0100

    some progress

--- app/src/main/java/com/example/livesportsbettingtips/navigation/NavHost.kt
+++ app/src/main/java/com/example/livesportsbettingtips/navigation/NavHost.kt
@@ -2,26 +2,38 @@ package com.example.livesportsbettingtips.navigation
 
 import android.annotation.SuppressLint
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.livesportsbettingtips.ui.screens.NavGraphs
+import com.example.livesportsbettingtips.ui.theme.Primary
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalAnimationApi::class, ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalAnimationApi::class, ExperimentalMaterial3Api::class,
+    ExperimentalMaterialNavigationApi::class
+)
 @Composable
 fun NavHost() {
     val navController = rememberAnimatedNavController()
-    Scaffold {
-        //    DestinationsNavHost(
-//        navGraph = NavGraphs.root,
-//        navController = navController,
-//        modifier = Modifier.weight(1f),
-//        engine = rememberAnimatedNavHostEngine()
-//    )
+    Scaffold(
+        topBar = {
+            Row(modifier = Modifier.background(Primary)) {
+
+            }
+        }
+    ) {
+        DestinationsNavHost(
+            navGraph = NavGraphs.root,
+            navController = navController,
+            engine = rememberAnimatedNavHostEngine()
+        )
     }
 
 }
