bc4b96c4bbec7ff3da9ae054d5af317e5793d92a
dinah <dinahb1407@gmail.com>
2023-07-19 15:42:06 +0100

    progress

--- app/src/main/java/com/example/livesportsbettingtips/MainActivity.kt
+++ app/src/main/java/com/example/livesportsbettingtips/MainActivity.kt
@@ -19,25 +19,18 @@ class MainActivity : ComponentActivity() {
             LivesportsBettingTipsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    FreeItem()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LivesportsBettingTipsTheme {
-        Greeting("Android")
     }
 }
