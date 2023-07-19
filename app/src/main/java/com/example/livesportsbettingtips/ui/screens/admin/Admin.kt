eee18d1fa0a82c48856653878b757abe60f002f7
Vcky4 <okon.victor.u@gmail.com>
2023-07-19 13:04:11 +0100

    create screens

---
+++ app/src/main/java/com/example/livesportsbettingtips/ui/screens/admin/Admin.kt
@@ -0,0 +1,8 @@
+package com.example.livesportsbettingtips.ui.screens.admin
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Admin(){
+
+}
---
+++ app/src/main/java/com/example/livesportsbettingtips/ui/screens/vip/Vip.kt
@@ -0,0 +1,8 @@
+package com.example.livesportsbettingtips.ui.screens.vip
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Vip(){
+
+}
