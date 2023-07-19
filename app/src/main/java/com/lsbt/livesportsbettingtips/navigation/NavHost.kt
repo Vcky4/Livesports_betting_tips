a84d54898e67f9272f0931423ab2fbeba177c687
Vcky4 <okon.victor.u@gmail.com>
2023-07-19 17:59:35 +0100

    app bar done

--- app/src/main/java/com/lsbt/livesportsbettingtips/navigation/NavHost.kt
+++ app/src/main/java/com/lsbt/livesportsbettingtips/navigation/NavHost.kt
@@ -3,18 +3,25 @@ package com.lsbt.livesportsbettingtips.navigation
 import android.annotation.SuppressLint
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import com.lsbt.livesportsbettingtips.ui.screens.NavGraphs
 import com.lsbt.livesportsbettingtips.ui.theme.Primary
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
+import com.lsbt.livesportsbettingtips.R
+import com.lsbt.livesportsbettingtips.ui.theme.Background
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
 
@@ -28,11 +35,30 @@ fun NavHost() {
     val navController = rememberAnimatedNavController()
     Scaffold(
         topBar = {
-            Row(modifier = Modifier.background(Primary)) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Primary),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
                 IconButton(onClick = { /*TODO*/ }) {
                     Icon(
-                        painter = painterResource(id = 0),
-                        contentDescription = "menu"
+                        painter = painterResource(id = R.drawable.menu),
+                        contentDescription = "menu",
+                        tint = Background
+                    )
+                }
+                Text(
+                    "Live Sports Betting Tips",
+                    color = Background,
+                    fontWeight = FontWeight.Bold
+                )
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.notifications),
+                        contentDescription = "notification",
+                        tint = Background
                     )
                 }
             }
