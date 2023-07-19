f454a050d232888dd869208a0de5ff8a5415f7cd
Vcky4 <okon.victor.u@gmail.com>
2023-07-19 18:19:37 +0100

    little

--- app/src/main/java/com/lsbt/livesportsbettingtips/navigation/NavHost.kt
+++ app/src/main/java/com/lsbt/livesportsbettingtips/navigation/NavHost.kt
@@ -6,7 +6,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -14,15 +17,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.lsbt.livesportsbettingtips.ui.screens.NavGraphs
 import com.lsbt.livesportsbettingtips.ui.theme.Primary
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.lsbt.livesportsbettingtips.R
 import com.lsbt.livesportsbettingtips.ui.theme.Background
+import com.lsbt.livesportsbettingtips.ui.theme.Secondary
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
 
@@ -64,7 +70,28 @@ fun NavHost() {
                     )
                 }
             }
-        }
+        },
+        floatingActionButton = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .background(Secondary, RoundedCornerShape(50))
+                    .clip(RoundedCornerShape(50))
+                    .padding(vertical = 14.dp, horizontal = 20.dp)
+            ) {
+                Text(
+                    "VIP",
+                    color = Background,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Icon(
+                    painter = painterResource(id = R.drawable.arrow_forward),
+                    contentDescription = "add",
+                    tint = Background
+                )
+            }
+        },
     ) {
         DestinationsNavHost(
             navGraph = NavGraphs.root,
