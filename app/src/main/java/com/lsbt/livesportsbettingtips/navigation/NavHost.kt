9694c585345938aa86af5d5532128f73755e7881
Vcky4 <okon.victor.u@gmail.com>
2023-07-20 21:17:37 +0100

    navigate drawer

--- app/src/main/java/com/lsbt/livesportsbettingtips/navigation/NavHost.kt
+++ app/src/main/java/com/lsbt/livesportsbettingtips/navigation/NavHost.kt
@@ -12,13 +12,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -43,6 +47,7 @@ import com.lsbt.livesportsbettingtips.ui.theme.Secondary
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
 import com.ramcosta.composedestinations.navigation.navigate
+import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(
@@ -54,74 +59,85 @@ fun NavHost() {
     val navController = rememberAnimatedNavController()
     val currentDestination: Destination = navController.appCurrentDestinationAsState().value
         ?: NavGraphs.root.startAppDestination
-    Scaffold(
-        topBar = {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Primary),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.menu),
-                        contentDescription = "menu",
-                        tint = Background
-                    )
-                }
-                Text(
-                    "Live Sports Betting Tips",
-                    color = Background,
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Bold
-                )
-                IconButton(onClick = { navController.navigate(NotificationsDestination) }) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.notifications),
-                        contentDescription = "notification",
-                        tint = Background
-                    )
-                }
-            }
-        },
-        floatingActionButton = {
-            AnimatedVisibility(currentDestination == FreeDestination) {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = { /*TODO*/ }) {
+        Scaffold(
+            topBar = {
                 Row(
-                    verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier
-                        .clickable { navController.navigate(VipDestination) }
-                        .background(Secondary, RoundedCornerShape(50))
-                        .clip(RoundedCornerShape(50))
-                        .padding(vertical = 14.dp, horizontal = 20.dp)
+                        .fillMaxWidth()
+                        .background(Primary),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
+                    IconButton(onClick = {
+                        scope.launch {
+                            drawerState.open()
+                        }
+                    }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.menu),
+                            contentDescription = "menu",
+                            tint = Background
+                        )
+                    }
                     Text(
-                        "VIP",
+                        "Live Sports Betting Tips",
                         color = Background,
-                        fontSize = 20.sp,
+                        fontSize = 16.sp,
                         fontWeight = FontWeight.Bold
                     )
-                    Icon(
-                        painter = painterResource(id = R.drawable.arrow_forward),
-                        contentDescription = "add",
-                        tint = Background
-                    )
+                    IconButton(onClick = { navController.navigate(NotificationsDestination) }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.notifications),
+                            contentDescription = "notification",
+                            tint = Background
+                        )
+                    }
                 }
-            }
-        },
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.bacf),
-            contentDescription = "background",
-            modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.FillWidth
-        )
-        DestinationsNavHost(
-            navGraph = NavGraphs.root,
-            navController = navController,
-            engine = rememberAnimatedNavHostEngine(),
-            modifier = Modifier.padding(it),
-        )
+            },
+            floatingActionButton = {
+                AnimatedVisibility(currentDestination == FreeDestination) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .clickable { navController.navigate(VipDestination) }
+                            .background(Secondary, RoundedCornerShape(50))
+                            .clip(RoundedCornerShape(50))
+                            .padding(vertical = 14.dp, horizontal = 20.dp)
+                    ) {
+                        Text(
+                            "VIP",
+                            color = Background,
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Icon(
+                            painter = painterResource(id = R.drawable.arrow_forward),
+                            contentDescription = "add",
+                            tint = Background
+                        )
+                    }
+                }
+            },
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.bacf),
+                contentDescription = "background",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.FillWidth
+            )
+            DestinationsNavHost(
+                navGraph = NavGraphs.root,
+                navController = navController,
+                engine = rememberAnimatedNavHostEngine(),
+                modifier = Modifier.padding(it),
+            )
+        }
     }
 
 }
