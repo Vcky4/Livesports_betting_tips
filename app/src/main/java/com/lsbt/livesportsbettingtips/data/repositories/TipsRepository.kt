c958c1e211daa899ee6b5d0d16cf5dce83a8f35f
Vcky4 <okon.victor.u@gmail.com>
2023-07-24 14:06:48 +0100

    repository fix

--- app/src/main/java/com/lsbt/livesportsbettingtips/data/repositories/TipsRepository.kt
+++ app/src/main/java/com/lsbt/livesportsbettingtips/data/repositories/TipsRepository.kt
@@ -2,20 +2,21 @@ package com.lsbt.livesportsbettingtips.data.repositories
 
 import com.lsbt.livesportsbettingtips.data.db.daos.TipDao
 import com.lsbt.livesportsbettingtips.data.db.models.HomeItemModel
+import com.lsbt.livesportsbettingtips.data.db.models.TipModel
 
 class TipsRepository(private val dao: TipDao) {
     //get all
     fun getAll() = dao.getAll()
 
     //create
-    suspend fun create(data: HomeItemModel) = dao.create(data)
+    suspend fun create(data: TipModel) = dao.create(data)
 
     //create many
-    suspend fun createMany(data: List<HomeItemModel>) = dao.createMany(data)
+    suspend fun createMany(data: List<TipModel>) = dao.createMany(data)
 
     //delete
-    suspend fun delete(data: HomeItemModel) = dao.delete(data)
+    suspend fun delete(data: TipModel) = dao.delete(data)
 
     //update
-    suspend fun update(data: HomeItemModel) = dao.update(data)
+    suspend fun update(data: TipModel) = dao.update(data)
 }
