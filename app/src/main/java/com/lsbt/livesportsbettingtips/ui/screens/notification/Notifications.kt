3d2c7f61f9d7efc52ad6e097daecd14962051959
Vcky4 <okon.victor.u@gmail.com>
2023-08-05 00:35:31 +0100

    post announcement done

--- app/src/main/java/com/lsbt/livesportsbettingtips/ui/screens/notification/Notifications.kt
+++ app/src/main/java/com/lsbt/livesportsbettingtips/ui/screens/notification/Notifications.kt
@@ -1,6 +1,9 @@
 package com.lsbt.livesportsbettingtips.ui.screens.notification
 
+import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -21,12 +25,14 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -47,13 +53,11 @@ import org.koin.androidx.compose.koinViewModel
 @Composable
 fun Notifications(navigator: DestinationsNavigator) {
     val viewModel: AdminViewModel = koinViewModel()
+    val announcement = viewModel.announcement.observeAsState("").value
     var title by remember {
-        mutableStateOf("")
+        mutableStateOf(announcement)
     }
-    var body by remember {
-        mutableStateOf("")
-    }
-
+    val context = LocalContext.current
     var processing by remember {
         mutableStateOf(false)
     }
@@ -62,6 +66,11 @@ fun Notifications(navigator: DestinationsNavigator) {
             Modifier
                 .background(Secondary, shape = MaterialTheme.shapes.medium)
                 .fillMaxWidth(0.9f)
+//                .fillMaxHeight(0.7f)
+                .scrollable(
+                    orientation = Orientation.Vertical,
+                    state = rememberScrollState()
+                )
                 .padding(vertical = 50.dp, horizontal = 16.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
@@ -121,9 +130,18 @@ fun Notifications(navigator: DestinationsNavigator) {
                 onClick = {
                     processing = true
                     viewModel.setAnnouncement(title)
-                    title = ""
-                    body = ""
-                    processing = false
+                        .addOnSuccessListener {
+                            Toast.makeText(context, "Saved", Toast.LENGTH_SHORT).show()
+                            processing = false
+                            navigator.popBackStack()
+                        }.addOnFailureListener {
+                            Toast.makeText(
+                                context,
+                                "Failed: ${it.localizedMessage}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            processing = false
+                        }
                 },
                 colors = ButtonDefaults.buttonColors(
                     containerColor = TextDeep,
