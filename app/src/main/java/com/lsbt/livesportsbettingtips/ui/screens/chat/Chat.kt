bb8c3e3088ad9bd740db5e3821325cea6da424a5
Vcky4 <okon.victor.u@gmail.com>
2023-08-23 00:57:03 +0100

    create chat screen

---
+++ app/src/main/java/com/lsbt/livesportsbettingtips/ui/screens/chat/Chat.kt
@@ -0,0 +1,10 @@
+package com.lsbt.livesportsbettingtips.ui.screens.chat
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+@Destination
+@Composable
+fun Chat(navigator: DestinationsNavigator) {
+}
