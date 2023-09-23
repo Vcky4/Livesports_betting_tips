b37870bf3ef031cf3a8e21a2370daa5e453ec4c7
Vcky4 <okon.victor.u@gmail.com>
2023-09-23 09:41:49 +0100

    upgrade version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.lsbt.livesportsbettingtips"
         minSdk = 21
         targetSdk = 33
-        versionCode = 7
-        versionName = "1.1.4"
+        versionCode = 8
+        versionName = "1.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
