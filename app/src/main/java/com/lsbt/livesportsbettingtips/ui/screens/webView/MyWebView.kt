5498a3ab79725884fbe414dee4f8dad302df7616
vcky4 <okon.victor.u@gmail.com>
2023-11-30 22:43:22 +0100

    done

--- app/src/main/java/com/lsbt/livesportsbettingtips/ui/screens/webView/MyWebView.kt
+++ app/src/main/java/com/lsbt/livesportsbettingtips/ui/screens/webView/MyWebView.kt
@@ -46,6 +46,39 @@ fun MyWebView(
                 settings.setSupportZoom(true)
                 settings.builtInZoomControls = true
                 settings.displayZoomControls = false
+                settings.allowContentAccess = true
+                settings.allowFileAccess = true
+                settings.allowFileAccessFromFileURLs = true
+                settings.allowUniversalAccessFromFileURLs = true
+                settings.loadsImagesAutomatically = true
+                settings.cacheMode = android.webkit.WebSettings.LOAD_NO_CACHE
+                settings.mixedContentMode = android.webkit.WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+
+                //allow chrome client
+                webViewClient = object : WebViewClient() {
+                    override fun shouldOverrideUrlLoading(
+                        view: WebView?,
+                        url: String?
+                    ): Boolean {
+                        view?.loadUrl(url!!)
+                        return true
+                    }
+                }
+                webChromeClient = object : android.webkit.WebChromeClient() {
+                    override fun onCreateWindow(
+                        view: WebView?,
+                        isDialog: Boolean,
+                        isUserGesture: Boolean,
+                        resultMsg: android.os.Message?
+                    ): Boolean {
+                        val newWebView = WebView(context)
+                        val transport = resultMsg?.obj as WebView.WebViewTransport
+                        transport.webView = newWebView
+                        resultMsg.sendToTarget()
+                        return true
+                    }
+                }
+
                 loadUrl(url)
                 webView = this
             }
