1070c06d2683f412488d23e9fac158fa8c685835
d1saym4n <disemfoneffiong@gmail.com>
2024-03-24 21:51:42 +0100

    feet: inactive and active color

---
+++ app/src/main/java/com/lsbt/livesportsbettingtips/components/Button2.kt
@@ -0,0 +1,50 @@
+package com.lsbt.livesportsbettingtips.components
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Button2(
+    modifier: Modifier = Modifier,
+    text: String,
+    processing: Boolean = false,
+    enabled: Boolean = true,
+    color: Color = Color.Black,
+    contentColor: Color = Color.White,
+    onClick: () -> Unit
+) {
+    Button(
+        enabled = !processing && enabled,
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = color,
+            contentColor = contentColor,
+            disabledContainerColor = color.copy(alpha = 0.5f),
+            disabledContentColor = Color.White
+        ),
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp),
+    ) {
+        if (processing) {
+            CircularProgressIndicator(
+                color = Color.White,
+                modifier = Modifier
+                    .size(32.dp)
+                    .align(Alignment.CenterVertically),
+                strokeWidth = 2.dp,
+            )
+        } else {
+            Text(text = text, style = MaterialTheme.typography.bodyLarge, color = Color.White)
+        }
+    }
+}
--- app/src/main/java/com/lsbt/livesportsbettingtips/ui/screens/home/Details.kt
+++ app/src/main/java/com/lsbt/livesportsbettingtips/ui/screens/home/Details.kt
@@ -1,18 +1,32 @@
 package com.lsbt.livesportsbettingtips.ui.screens.home
 
 import android.text.format.DateUtils
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -23,10 +37,13 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.lsbt.livesportsbettingtips.R
+import com.lsbt.livesportsbettingtips.ui.theme.Primary
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.rizzi.bouquet.PageContentInt
 import org.koin.androidx.compose.koinViewModel
 
+@OptIn(ExperimentalFoundationApi::class, ExperimentalStdlibApi::class)
 @Destination
 @Composable
 fun DetailScreen(trigger: String, navigator: DestinationsNavigator) {
@@ -34,15 +51,37 @@ fun DetailScreen(trigger: String, navigator: DestinationsNavigator) {
     LaunchedEffect(key1 = viewModel) {
         viewModel.getTips(trigger)
     }
+    val tabItems = listOf(
+        TabItem(
+            title = "Today"
+        ),
+        TabItem(
+            title = "History"
+        ),
+    )
     val tips = viewModel.tips.observeAsState(listOf()).value
+    val pagerState = rememberPagerState(
+        0
+    )
     val prev = stringResource(id = R.string.previous_correct_score)
     val prev2 = stringResource(id = R.string.previous_draws_results)
+
+    var selectedTabIndex by remember {
+        mutableStateOf(0)
+    }
+    LaunchedEffect(selectedTabIndex) {
+        pagerState.animateScrollToPage(selectedTabIndex)
+    }
+    LaunchedEffect(pagerState.currentPage) {
+        selectedTabIndex = pagerState.currentPage
+    }
     val history = when (trigger) {
         prev -> tips.sortedByDescending { it.date }
         prev2 -> tips.sortedByDescending { it.date }
         else -> tips.filter { !DateUtils.isToday(it.date) }.sortedByDescending { it.date }
     }
-    Column {
+
+    Column (modifier = Modifier.fillMaxSize()) {
         Row(
             modifier = Modifier.padding(vertical = 14.dp),
             verticalAlignment = Alignment.CenterVertically
@@ -66,69 +105,103 @@ fun DetailScreen(trigger: String, navigator: DestinationsNavigator) {
                 modifier = Modifier.padding(start = 16.dp)
             )
         }
-        LazyColumn {
+        HorizontalPager(
+            state = pagerState,
+            pageCount = 2,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) { index ->
+            when (index) {
+                0 -> LazyColumn {
+                    if (trigger != prev && trigger != prev2) {
 
-            if (trigger != prev && trigger != prev2) {
-                item {
-                    Text(
-                        text = stringResource(id = R.string.today),
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.White,
-                        modifier = Modifier.padding(start = 16.dp)
-                    )
-                }
-                if (tips.none { DateUtils.isToday(it.date) }) {
-                    item {
-                        Text(
-                            text = stringResource(id = R.string.no_tips_available),
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Color.White,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(horizontal = 16.dp, vertical = 30.dp),
-                            textAlign = TextAlign.Center
-                        )
-                    }
-                } else {
-                    items(
-                        items = tips.filter { DateUtils.isToday(it.date) }
-                    ) {
-                        DetailItem(it)
+                        if (tips.none { DateUtils.isToday(it.date) }) {
+                            item {
+                                Text(
+                                    text = stringResource(id = R.string.no_tips_available),
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    color = Color.White,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(horizontal = 16.dp, vertical = 30.dp),
+                                    textAlign = TextAlign.Center
+                                )
+                            }
+                        } else {
+                            items(
+                                items = tips.filter { DateUtils.isToday(it.date) }
+                            ) {
+                                DetailItem(it)
+                            }
+                        }
                     }
                 }
+
+                else -> LazyColumn {
+                    if (history.isEmpty()) {
+                        item {
+                            Text(
+                                text = stringResource(id = R.string.no_tips_available),
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.White,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 16.dp, vertical = 30.dp),
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    } else {
+                        items(
+                            items = history
+                        ) {
+                            DetailItem(it)
+                        }
+                    }
+
+                }
+
             }
-            item {
-                Text(
-                    text = stringResource(id = R.string.history),
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.White,
-                    modifier = Modifier.padding(start = 16.dp)
-                )
-            }
-            if (history.isEmpty()) {
-                item {
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .align(Alignment.CenterHorizontally),
+            verticalAlignment = Alignment.Bottom
+        ) {
+            tabItems.forEachIndexed { index, item ->
+                val isSelected = selectedTabIndex == index
+                val backgroundColor = if (isSelected) Primary else Color.Unspecified
+                Box(modifier = Modifier
+                    .weight(0.5f)
+                    .height(60.dp)
+                    .clickable { selectedTabIndex = index }
+                    .background(color = backgroundColor),
+                    contentAlignment = Alignment.Center
+                ) {
                     Text(
-                        text = stringResource(id = R.string.no_tips_history_available),
+                        text = item.title,
                         fontSize = 20.sp,
                         fontWeight = FontWeight.Bold,
                         color = Color.White,
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 16.dp, vertical = 30.dp),
+                            .fillMaxWidth(),
                         textAlign = TextAlign.Center
                     )
                 }
-            } else {
-                items(
-                    items = history
-                ) {
-                    DetailItem(it)
-                }
+
             }
         }
     }
 
 }
+
+
+data class TabItem(
+    val title: String
+)
+
+
